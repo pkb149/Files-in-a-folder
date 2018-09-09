c742b02f645d4194c61d51e24d7643a544ea3d18
Prashant Bharadwaj <pkb149@hotmail.com>
2018-09-09 11:10:36 +0530

    Update files.py

--- files.py
+++ files.py
@@ -5,15 +5,18 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("mount")
 args = parser.parse_args()
-results =subprocess.check_output("sudo find "+args.mount+" -xdev -type f -printf '%p %s\t'", shell=True)
-results=results.split("\t")
-list =[]
-for i in range(results.__len__()-1):
-    l=results[i]
-    l=l.split(" ")
-    a=[l[0],l[1]]
-    list.append(a)
 dict={}
-dict["files"]=list
+try:
+    results =subprocess.check_output("sudo find "+args.mount+" -xdev -type f -printf '%p %s\t'", shell=True)
+    results=results.split("\t")
+    list =[]
+    for i in range(results.__len__()-1):
+        l=results[i]
+        l=l.split(" ")
+        a=[l[0],int(l[1])]
+        list.append(a)    
+    dict["files"]=list
+except:
+    dict["error"]="No such file or directory"
 json_data = json.dumps(dict, sort_keys=True, indent=4)
 print(json_data)
